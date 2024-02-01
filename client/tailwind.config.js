b61c900c2065589770adf8a7a0c6a1d446f52781
SPYKAR-KING <80270746+SPYKAR-KING@users.noreply.github.com>
2024-02-01 20:54:58 +0530

    Project complete

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: '',
+  content: {
+      enabled: true,
+      content: [
+          './src/**/*.{html,ts,jsx}',
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+  }
+}
+
+
